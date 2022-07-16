1a3a8dca1e870400f54010ad3d9ce22c9b94ce61
Ekaterina <eka.kachan@gmail.com>
2022-07-16 21:54:08 +0400

    feat: add individual remove button for each invoice items

--- src/components/InvoiceForm/InvoiceForm.tsx
+++ src/components/InvoiceForm/InvoiceForm.tsx
@@ -1,65 +1,207 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "../../App.scss";
-import InvoiceFormItem from "./InvoiceFormItem";
+import closeIcon from "../../assets/icon/icon-close.svg";
+
+interface InvoiceValues {
+  invoiceNumber: number;
+  invoiceDate: number;
+  invoiceDue: number;
+  item: string;
+  hours: string;
+  rate: string;
+  subtotal: string;
+}
+interface InvoiceTableValues {
+  item: string;
+  hours: string;
+  rate: string;
+  subtotal: string;
+}
 
 function InvoiceItem() {
   let dateNow = new Date().toISOString().split("T")[0];
-  const [invoiceList, setInvoiceList] = useState([<InvoiceFormItem/>] as Array);
+
+  const [tableFields, setTableFields] = useState([
+    { item: "", hours: "", rate: "", subtotal: "" },
+  ]);
+
   function addInvoiceItem() {
-    setInvoiceList(invoiceList.concat(<InvoiceFormItem key={invoiceList.length}/>));
+    setTableFields([
+      ...tableFields,
+      {
+        item: "",
+        hours: "",
+        rate: "",
+        subtotal: "",
+      },
+    ]);
   }
-  function deleteLastItem(){
-    setInvoiceList(invoiceList.slice(0, -1));
+  function deleteLastItem() {
+    setTableFields(tableFields.slice(0, -1));
   }
+
+  function removeTableItem(index: any) {
+    const rows = [...tableFields];
+    rows.splice(index, 1);
+    setTableFields(rows);
+  }
+
+  function handleChange(
+    index: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    const { name, value } = event.target;
+    const list: any = [...tableFields];
+    list[index][name] = value;
+    setTableFields(list);
+  }
+
+  function submitInvoiceItem(data: any) {
+    console.log(JSON.stringify(tableFields));
+    console.log(JSON.stringify(data))
+  }
+
+  const { register, handleSubmit } = useForm();
 
   return (
     <div className="invoice-wrapper__form-card">
       <h3 className="invoice-title__from">Invoice info</h3>
       <div className="invoice-info">
-        <div className="invoice-input__text">
-          <label>Invoice Number:</label>
-          <input
-            type="text"
-            required
-            title="Click to change number"
-            value="001"
-          />
-        </div>
-        <div className="invoice-input__date">
-          <label>Invoice Date:</label>
-          <input
-            type="date"
-            required
-            title="Click to change date"
-            value={dateNow}
-          />
-        </div>
-        <div className="invoice-input__date">
-          <label>Due:</label>
-          <input
-            type="date"
-            required
-            title="Click to change date"
-            value={dateNow}
-          />
-        </div>
-        <div className="invoice-table">
-          <table>
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Hours</th>
-                <th>Rate</th>
-                <th>Subtotal</th>
-              </tr>
-            </thead>
-            <tbody>
-              {invoiceList} 
-            </tbody>
-          </table>
-          {invoiceList.length >= 2 ? <button className="btn-delete" onClick={deleteLastItem}>Delete Last Item</button> : <div></div>}
-          <button onClick={addInvoiceItem}>Add New Invoice Item</button>
-        </div>
+        <form onSubmit={handleSubmit(submitInvoiceItem)}>
+          <div className="invoice-input__text">
+            <label>Invoice Number:</label>
+            <input
+              type="text"
+              required
+              title="Click to change number"
+              placeholder="001"
+              {...register("invoiceNumber", { required: true })}
+            />
+          </div>
+          <div className="invoice-input__date">
+            <label>Invoice Date:</label>
+            <input
+              type="date"
+              required
+              title="Click to change date"
+              placeholder={dateNow}
+              {...register("invoiceDate", { required: true })}
+            />
+          </div>
+          <div className="invoice-input__date">
+            <label>Due:</label>
+            <input
+              type="date"
+              required
+              title="Click to change date"
+              placeholder={dateNow}
+              {...register("invoiceDue", { required: true })}
+            />
+          </div>
+          <div className="invoice-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Hours</th>
+                  <th>Rate</th>
+                  <th>Subtotal</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableFields.map((data, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>
+                        <div className="invoice-input__text">
+                          <input
+                            type="text"
+                            required
+                            value={data.item || ""}
+                            title="Click to change number"
+                            placeholder="Set up ads"
+                            name="item"
+                            onChange={(event) => handleChange(index, event)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="invoice-input__text">
+                          <input
+                            type="text"
+                            required
+                            value={data.hours || ""}
+                            title="Click to change number"
+                            placeholder="3h"
+                            name="hours"
+                            onChange={(event) => handleChange(index, event)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="invoice-input__text">
+                          <input
+                            type="text"
+                            required
+                            value={data.rate || ""}
+                            title="Click to change number"
+                            placeholder="10$"
+                            name="rate"
+                            onChange={(event) => handleChange(index, event)}
+                          />
+                        </div>
+                      </td>
+                      <td>
+                        <div className="invoice-input__text">
+                          <input
+                            type="text"
+                            required
+                            value={data.subtotal || ""}
+                            title="Click to change number"
+                            placeholder="30$"
+                            name="subtotal"
+                            onChange={(event) => handleChange(index, event)}
+                          />
+                        </div>
+                      </td>
+                      {tableFields.length >= 2 ? (
+                        <td>
+                          <button
+                            onClick={removeTableItem}
+                            className="btn-remove"
+                          >
+                            <img src={closeIcon} />
+                          </button>
+                        </td>
+                      ) : (
+                        <td></td>
+                      )}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            {tableFields.length >= 2 ? (
+              <button
+                className="button-style btn-delete"
+                onClick={deleteLastItem}
+              >
+                Delete Last Item
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <div>
+              <button className="button-style" onClick={addInvoiceItem}>
+                Add New Invoice Item
+              </button>
+            </div>
+            <button className="button-style" type="submit">
+              Submit Invoice Item
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
